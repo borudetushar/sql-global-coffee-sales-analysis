c0b17fdaa01c7cdd60170332350c4a5ad8c25900
Tushar Borude <borudetushar77@gmail.com>
2025-07-24 12:04:52 +0530

    Add files via upload

---
+++ data/coffee_schema.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE `your_project.your_dataset.coffee_sales` (
+  order_id INT64,
+  order_date DATE,
+  region STRING,
+  country STRING,
+  store_name STRING,
+  product STRING,
+  category STRING,
+  quantity INT64,
+  unit_price FLOAT64,
+  total_sales FLOAT64,
+  cost FLOAT64,
+  profit FLOAT64
+);
